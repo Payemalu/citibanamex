e26a84fd43059c6fe8987c8968e4c803e8aa65dd
Juan Carlos Cuellar Ruz <jc.cuellar.ruz@gmail.com>
2021-03-20 01:38:44 +0100

    First commit to show the Citibanamex exam

---
+++ README.md
@@ -0,0 +1,7 @@
+# exam-citibank
+
+Angular 8 User Registration and Login Example with Webpack 4
+
+npm install
+
+npm start run
---
+++ src/app/_models/user.ts
@@ -0,0 +1,13 @@
+﻿import { DecimalPipe } from "@angular/common";
+
+export class User {
+    id: number;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    description: string;
+    amount: number;
+    status: boolean;
+    token: string;
+}
